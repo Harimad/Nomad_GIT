f5c0af1431d1b85a39cc3599b6746116156f7f86
Tim Lee <leesh68481@gmail.com>
2022-01-18 08:35:48 +0000

    7.3 Saving To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,21 +2,41 @@ const todoForm = document.querySelector('#todo-form');
 const todoInput = document.querySelector('#todo-form input');
 const todoList = document.querySelector('#todo-list');
 
+const toDos = [];
+
 function handleTodoForm(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = '';
   todoInput.focus();
   
+  toDos.push(newTodo);
+  saveToDos();
+
   paintToDo(newTodo);
 }
 
-function paintToDo(newTodo) {
-  const newList = document.createElement('li')
-  const newSpan = document.createElement('span');
-  newList.appendChild(newSpan);
-  newList.style.listStyleType = 'none'; //li 스타일제거
-  newSpan.innerText = newTodo;
-  todoList.appendChild(newList);
+function saveToDos() { //toDos를 localStorage에 넣는 함수
+  localStorage.setItem("todos", JSON.stringify(toDos));
 }
+
+function paintToDo(newTodo) {
+  const li = document.createElement('li')
+  const span = document.createElement('span');
+  li.style.listStyleType = 'none'; //li 스타일제거
+  span.innerText = newTodo;
+  const btn = document.createElement('button');
+  btn.innerText = '❌'
+  btn.addEventListener('click', deleteTodo);
+
+  li.appendChild(span);
+  li.appendChild(btn);
+  todoList.appendChild(li);
+}
+
+function deleteTodo(event) {
+  const element = event.target.parentNode;
+  element.remove();
+}
+
 todoForm.addEventListener('submit', handleTodoForm);
